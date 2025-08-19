57b003b402168e77e62419939abecb106933f5c8
Francesco <inerba@gmail.com>
2025-08-19 15:02:26 +0200

    Aggiunta documentazione per la funzione db_config nel file helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,6 +1,13 @@
 <?php
 
 if (! function_exists('db_config')) {
+    /**
+     * Retrieve a configuration value from the database.
+     *
+     * @param  string  $key  The configuration key.
+     * @param  mixed  $default  The default value to return if the configuration key is not found.
+     * @return mixed The configuration value.
+     */
     function db_config(string $key, mixed $default = null): mixed
     {
         return \Postare\DbConfig\DbConfig::get($key, $default);
